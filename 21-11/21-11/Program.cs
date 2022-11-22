199de178b8dd18aeb2d9b11dd094401fdcd16605
AmerAmora <Amer.m.amora@gmail.com>
2022-11-22 12:38:26 +0300

    Upload

--- 21-11/21-11/Program.cs
+++ 21-11/21-11/Program.cs
@@ -11,86 +11,86 @@ namespace _21_11
         static void Main(string[] args)
         {
 
-            ////Task1
-            Console.WriteLine("Enter first number");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter second number");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            if (num1 > num2) Console.WriteLine(num1);
-            else if (num2 > num1) Console.WriteLine(num2);
-            else Console.WriteLine("Equal numbers");
+            //////Task1
+            //Console.WriteLine("Enter first number");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter second number");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //if (num1 > num2) Console.WriteLine(num1);
+            //else if (num2 > num1) Console.WriteLine(num2);
+            //else Console.WriteLine("Equal numbers");
 
-            ////Task 2 
-            Console.WriteLine("Enter singed number");
-            int t2num = Convert.ToInt32(Console.ReadLine());
-            if (t2num > 0) Console.WriteLine("+");
-            else Console.WriteLine("-");
-            ////Task3
-            int t3num1 = Convert.ToInt32(Console.ReadLine());
-            int t3num2 = Convert.ToInt32(Console.ReadLine());
-            int t3num3 = Convert.ToInt32(Console.ReadLine());
+            //////Task 2 
+            //Console.WriteLine("Enter singed number");
+            //int t2num = Convert.ToInt32(Console.ReadLine());
+            //if (t2num > 0) Console.WriteLine("+");
+            //else Console.WriteLine("-");
+            //////Task3
+            //int t3num1 = Convert.ToInt32(Console.ReadLine());
+            //int t3num2 = Convert.ToInt32(Console.ReadLine());
+            //int t3num3 = Convert.ToInt32(Console.ReadLine());
 
-            if (t3num2 > t3num1 && t3num2 > t3num3)
-            {
-                if (t3num1 > t3num3)
-                    Console.WriteLine(t3num2 + " " + t3num1 + " " + t3num3);
-                else
-                {
-                    Console.WriteLine(t3num2 + " " + t3num3 + " " + t3num1);
-                }
+            //if (t3num2 > t3num1 && t3num2 > t3num3)
+            //{
+            //    if (t3num1 > t3num3)
+            //        Console.WriteLine(t3num2 + " " + t3num1 + " " + t3num3);
+            //    else
+            //    {
+            //        Console.WriteLine(t3num2 + " " + t3num3 + " " + t3num1);
+            //    }
 
-            }
-            else if (t3num3 > t3num1 && t3num3 > t3num2)
-            {
+            //}
+            //else if (t3num3 > t3num1 && t3num3 > t3num2)
+            //{
 
-                if (t3num1 > t3num2)
-                    Console.WriteLine(t3num3 + " " + t3num1 + " " + t3num2);
-                else
-                {
-                    Console.WriteLine(t3num3 + " " + t3num2 + " " + t3num1);
-                }
-            }
-            else if (t3num1 > t3num2 && t3num1 > t3num3)
-            {
+            //    if (t3num1 > t3num2)
+            //        Console.WriteLine(t3num3 + " " + t3num1 + " " + t3num2);
+            //    else
+            //    {
+            //        Console.WriteLine(t3num3 + " " + t3num2 + " " + t3num1);
+            //    }
+            //}
+            //else if (t3num1 > t3num2 && t3num1 > t3num3)
+            //{
 
-                if (t3num2 > t3num3)
-                    Console.WriteLine(t3num1 + " " + t3num2 + " " + t3num3);
-                else
-                {
-                    Console.WriteLine(t3num1 + " " + t3num3 + " " + t3num2);
-                }
-            }
-            ////Task4
-            int[] t4num = new int[5];
-            Console.WriteLine("enter num 1");
-            t4num[0] = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enter num 2");
-            t4num[1] = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enter num 3");
-            t4num[2] = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enter num 4");
-            t4num[3] = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enter num 5");
-            t4num[4] = Convert.ToInt32(Console.ReadLine());
-            int max = t4num[0];
-            if (max < t4num[1]) max = t4num[1];
-            if (max < t4num[2]) max = t4num[2];
-            if (max < t4num[3]) max = t4num[3];
-            if (max < t4num[4]) max = t4num[4];
-            Console.WriteLine(max);
+            //    if (t3num2 > t3num3)
+            //        Console.WriteLine(t3num1 + " " + t3num2 + " " + t3num3);
+            //    else
+            //    {
+            //        Console.WriteLine(t3num1 + " " + t3num3 + " " + t3num2);
+            //    }
+            //}
+            //////Task4
+            //int[] t4num = new int[5];
+            //Console.WriteLine("enter num 1");
+            //t4num[0] = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("enter num 2");
+            //t4num[1] = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("enter num 3");
+            //t4num[2] = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("enter num 4");
+            //t4num[3] = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("enter num 5");
+            //t4num[4] = Convert.ToInt32(Console.ReadLine());
+            //int max = t4num[0];
+            //if (max < t4num[1]) max = t4num[1];
+            //if (max < t4num[2]) max = t4num[2];
+            //if (max < t4num[3]) max = t4num[3];
+            //if (max < t4num[4]) max = t4num[4];
+            //Console.WriteLine(max);
 
 
-            //Task 5
-            Console.WriteLine("Enter number in kilometers");
-            double t5num = Convert.ToDouble(Console.ReadLine());
-            t5num = t5num * 0.62137119;
-            Console.WriteLine($"Number in miles  {t5num}");
-            //Task 6
-            Console.WriteLine("Enter the number of hours");
-            int hours = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the number of minutes");
-            int minutes = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(hours * 60 + minutes);
+            ////Task 5
+            //Console.WriteLine("Enter number in kilometers");
+            //double t5num = Convert.ToDouble(Console.ReadLine());
+            //t5num = t5num * 0.62137119;
+            //Console.WriteLine($"Number in miles  {t5num}");
+            ////Task 6
+            //Console.WriteLine("Enter the number of hours");
+            //int hours = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the number of minutes");
+            //int minutes = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(hours * 60 + minutes);
             //Task 7
             Console.WriteLine("Enter number of minutes");
             int t7min = Convert.ToInt32(Console.ReadLine());
---
+++ 22-11/22-11/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1
+            int[] ARR = { 1, 7, 9, 45 };
+            string[] arr2 = { "Str", "alex", "moh" };
+            string[] arr3 = { "the", "fox" ,"over", "lazy", "dog" };
+            //Task 2
+            String[] fruits = { "Tomato", "Banana", "Watermelon" };
+            Console.WriteLine(fruits[1]+" "+ fruits[0]);
+            //Task 3
+            string[] foods = {"Burger","Mansaf","Hotdog","Mandi","Pasta" };
+            string[] Sports = { "Soccer", "Football", "basketball" };
+            string[] Movies = { "Fight club", "Seven", "Harry potter", "The maze runner" };
+            foreach (string food in foods) { Console.WriteLine(food); }
+            foreach (string sport in Sports) { Console.WriteLine(sport); }
+            foreach (string movie in Movies) { Console.WriteLine(movie); }
+            //Task 4
+            Console.WriteLine("Enter number separated by a comma");
+            string s = Console.ReadLine();
+            string[] subs = s.Split(',');
+            int[] numbers = new int[3];
+            int sum = 0;
+            for (int i = 0; i < 3; i++)
+            { numbers[i] = Convert.ToInt32(subs[i]); sum += numbers[i]; }
+            Console.WriteLine(sum);
+            //Task 5
+            int oddsum = 0;
+            for(int i=1;i<=100;i++)
+            { 
+                if ((i % 2)!= 0) { oddsum = oddsum + i; }
+                
+
+            }
+            Console.WriteLine(oddsum);
+            //Task 6
+            for (int row = 0; row <= 4; row++) 
+            
+            {
+                for (int col = 0; col <= row; col++)
+                { Console.Write("*"); }
+                Console.WriteLine();
+            
+            }
+            //Task 7
+            int counter=1;
+            for (int row = 0; row <= 4; row++)
+
+            {
+                for (int col = 0; col < row; col++)
+                { Console.Write(counter); counter++; }
+                Console.WriteLine("");
+
+            }
+
+        }
+    }
+}
