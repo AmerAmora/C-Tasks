af80c7b82401f3b09ef23870b52be09a57b05f02
AmerAmora <Amer.m.amora@gmail.com>
2022-11-28 16:35:23 +0300

    Upload

--- 27-11practice/27-11practice/Program.cs
+++ 27-11practice/27-11practice/Program.cs
@@ -6,55 +6,66 @@ using System.Threading.Tasks;
 
 namespace _27_11practice
 {
-    
-    internal class Program
+
+    // C# program to
+    // define Sealed Class
+    using System;
+
+    class Printer
     {
-        static double Add(double num1, double num2)
 
-        {  
-            return num1+num2; 
-        }
-        static double Sub(double num1, double num2)
+        // Display Function for
+        // Dimension printing
 
+        // Display Function
+        public virtual void print()
         {
-            return num1 - num2;
-        }
-        static double Mult(double num1, double num2)
-
-        {
-            return num1 * num2;
-        }
-        static double Div(double num1, double num2)
-
-        {
-            return num1 / num2;
-        }
-        static double Mod(double num1, double num2)
-
-        {
-            return num1 % num2;
-        }
-        static string Welcome( string name) 
-        { 
-            return "Welcome " + name; 
-        }
-        static string Goodbye(string name) 
-        { 
-            return "Goodbye " + name;
-        }
-        static void Main(string[] args)
-        {
-            int x = 5; 
-            int y = 5;
-            Console.WriteLine(Add(x,y));
-            Console.WriteLine(Sub(x, y));
-            Console.WriteLine(Mult(x, y));
-            Console.WriteLine(Div(x, y));
-            Console.WriteLine(Mod(x, y));
-            Console.WriteLine(Welcome("Amer"));
-            Console.WriteLine(Goodbye("Amer"));
-
-
+            Console.WriteLine("printer printing....\n");
         }
     }
+
+    // inheriting class
+    sealed class  LaserJet : Printer
+    {
+
+       
+        override public void print()
+        {
+            Console.WriteLine("Laserjet printer printing....\n");
+        }
+    }
+
+    // Officejet class cannot override show
+    // function as it is sealed in LaserJet class.
+    //class Officejet : LaserJet
+    //{ 
+    //    // can not override show function or else
+    //    // compiler error : 'Officejet.show()' :
+    //    // cannot override inherited member
+    //    // 'LaserJet.show()' because it is sealed.
+
+    //    override public void print()
+    //    {
+    //        Console.WriteLine("Officejet printer printing....");
+    //    }
+    //}
+
+    // Driver Class
+    class Program
+    {
+
+        // Driver Code
+        static void Main(string[] args)
+        {
+
+            Printer p = new Printer();
+            p.print();
+
+            Printer ls = new LaserJet();
+            ls.print();
+
+            
+        }
+    }
+
 }
---
+++ 28-11/28-11/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_11
+{ class Car 
+    
+    {
+        string make { get; set; }
+        int year { get; set; }
+        string type { get; set; }
+        int price { get; set; }
+        string model { get; set; }
+        string palletNo { get; set; }
+        string color { get; set; }
+        bool engineCheck=false;
+        public Car(string make,string type,string model,string palletNo,string color, int year , int price) 
+        {
+            this.make = make;
+            this.type = type;
+            this.model = model;
+            this.palletNo= palletNo;
+            this.color = color;
+            this.year = year;
+            this.price = price;
+        }
+        public void start() 
+        { 
+            Console.WriteLine("Engine started"); 
+            engineCheck= true;
+        }
+        public void stop() { 
+            if (!engineCheck) 
+
+                Console.WriteLine("Engine isn't on already");
+            else { 
+
+                Console.WriteLine("Engine stopped");
+                engineCheck= false;
+            }
+
+        }
+        public void print() { Console.WriteLine(make+ " " +type + " " + model + " " + palletNo + " " + color + " " + year + " " + price); }
+        public int Gas(int gasAmount) 
+        { int distance=gasAmount*20;
+            return distance; 
+        }
+    }
+    class bmw : Car
+    {
+        public bmw(string make, string type, string model, string palletNo, string color, int year, int price) : base(make, type, model, palletNo, color, year, price)
+{}
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bmw car = new bmw("Amer","BMW","BMW","JOD199922","Blue",2022,19999);
+            car.print();
+
+            car.stop();
+            car.start();
+            car.stop();
+        }
+    }
+}
