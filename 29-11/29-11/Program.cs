a7fe241cd8b2220773eb014bf65d1e4d4c28c20d
AmerAmora <Amer.m.amora@gmail.com>
2022-11-29 16:12:40 +0300

    Upload

---
+++ 29-11/29-11/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _29_11
+{
+    class Employee 
+    {
+        public string name { get; set; }
+        public int id { get; set; }
+        public DateTime dateofbirth { get; set; }
+        public Employee(string name,int id, int day,int month, int year) 
+        {   this.name = name;
+            this.id = id;
+            dateofbirth= new DateTime(year, month, day);
+
+        }
+
+        public void age() {
+            DateTime now= DateTime.Now; TimeSpan value = now.Subtract(dateofbirth);
+           
+            Console.WriteLine((int)value.TotalDays/365);
+
+        }
+        public virtual void print() { Console.WriteLine(name + " " + id + " " + dateofbirth); }
+    
+    }
+    class Manager : Employee
+    {
+        public Manager(string name, int id, int day, int month, int year) : base(name, id, day, month, year)
+        {
+        }
+        public override void print()
+        {
+              Console.WriteLine(name + " " + id + " " );
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Manager man = new Manager("Amer",3,18,8,1999);
+            man.print();
+            man.age();
+        }
+    }
+}
---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{ 
+   abstract class A {
+
+        public void StartEngine() { }
+        public void StopEngine() { }
+        public void OpenDoor() { }
+        public void CloseDoor() { }
+        public void Honk() { }
+        public void IncreaseSpeed() { }
+        public void DecreaseSpeed() { }
+        public void Breaks() { }
+        public void OpenWindow() { }
+        public void CloseWindow() { }
+        public void AdjustSeat() { }
+        public void CleanGlass() { }
+        public void ChangeGear() { }
+        public void startlights() { }
+        public void stoplights() { }
+
+        public void startAC() { }
+        public void stopAC() { }
+        public void startradio() { }
+        public void stopradio() { }
+
+
+
+    }
+    class B : A {
+       //override public void print() { }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            B amer=new B();
+        }
+    }
+}
